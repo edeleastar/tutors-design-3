dceedb911ad4730371fe180ff2762e57144d7e18
Eamonn de Leastar <edeleastar@wit.ie>
2019-07-21 15:23:47 +0100

    remove duplicated topic title

--- cypress/integration/tutors.js
+++ cypress/integration/tutors.js
@@ -9,11 +9,22 @@ describe("User page", () => {
 
   beforeEach(function() {
     cy.visit("http://localhost:8080/#course/tutors-starter.netlify.com");
+    //cy.visit("https://tutors-design-dev.netlify.com/#course/tutors-starter.netlify.com");
+    //cy.visit("https://tutors-design-prod.netlify.com/course/tutors-starter.netlify.com");
   });
 
   it("HomePage", function() {
     for (let [i, topic] of course.los.entries()) {
       cy.lo(i, "card-deck card", topic);
     }
+  });
+
+  it("Topics", function() {
+    for (let topic of course.los) {
+      cy.contains(topic.title).click({ force: true });
+      cy.wait(100);
+      cy.carddeck(topic.los, "card-deck card");
+      cy.home();
+    }ß
   });
 });
