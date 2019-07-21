b60f8b5e87023743b24d6f2c3ed4dce78a7e5979
Eamonn de Leastar <edeleastar@wit.ie>
2019-07-21 16:44:54 +0100

    test different card types

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -19,20 +19,20 @@ Cypress.Commands.add('home', (userType, options = {}) => {
 Cypress.Commands.add('lo', (index, selector, lo, icon, options = {}) => {
   cy.get(selector).eq(index).should('contain', lo.title);
   cy.get(selector).eq(index).should('contain', lo.summary);
-  // if (lo.type == "panelvideo") {
-  //   let parts = lo.video.split('/');;
-  //   const lastSegment = parts.pop() || parts.pop();
-  //   const url = "https://www.youtube.com/embed/" + lastSegment;
-  //   cy.get(selector).eq(index).find("iframe").should('have.attr', 'src', url);
-  // }
-  // else {
-  //   cy.get(selector).eq(index).find("img").should('have.attr', 'src', lo.img);
-  // }
-  // cy.get(selector).eq(index).find("svg").should('have.class', icons[lo.type]);
+  if (lo.type == "panelvideo") {
+    let parts = lo.video.split('/');;
+    const lastSegment = parts.pop() || parts.pop();
+    const url = "https://www.youtube.com/embed/" + lastSegment;
+    cy.get(selector).eq(index).find("iframe").should('have.attr', 'src', url);
+  }
+  else {
+    cy.get(selector).eq(index).find("img").should('have.attr', 'src', lo.img);
+  }
+  cy.get(selector).eq(index).find("svg").should('have.class', icons[lo.type]);
 });
 
 Cypress.Commands.add('carddeck', (los, selector, options = {}) => {
-  const carddeck = los;
+  const carddeck = los.filter(lo => lo.type != "panelvideo" && lo.type != "unit" && lo.type != "paneltalk");
   for (let [i, lo] of carddeck.entries()) {
     cy.lo(i, selector, lo);
   }
