0e46c8f5eaa919f799aa54618ed82acac73890f5
Eamonn de Leastar <edeleastar@wit.ie>
2019-07-21 15:09:21 +0100

    util functions for route management

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,4 +1,5 @@
-import {Lo} from "./lo";
+import { Lo } from "./lo";
+import environment from "../environment";
 
 export function injectCourseUrl(lo: Lo, url) {
   if (lo.route) lo.route = lo.route.replace("{{COURSEURL}}", url);
@@ -10,4 +11,19 @@ export function injectCourseUrl(lo: Lo, url) {
       injectCourseUrl(lo, url);
     });
   }
+}
+
+export function fixRoutes(lo: Lo) {
+  if (environment.pushState && lo.route && lo.route[0] == "#") {
+    lo.route = lo.route.slice(1);
+  }
+  if (environment.pushState && lo.video && lo.video[0] == "#") {
+    lo.video = lo.video.slice(1);
+  }
+}
+
+export function lastSegment(url: string) {
+  var parts = url.split("/");
+  var lastSegment = parts.pop() || parts.pop();
+  return lastSegment;
 }
