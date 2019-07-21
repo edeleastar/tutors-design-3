b2909e6595ac2528dfa609106391dc2bd4fe6a2a
Eamonn de Leastar <edeleastar@wit.ie>
2019-07-21 19:53:34 +0100

    wrapper around showdown

---
+++ src/services/markdown-parser.ts
@@ -0,0 +1,21 @@
+const showdown = require("showdown");
+
+const showdownHighlight = require("showdown-highlight");
+let converter = new showdown.Converter({
+  extensions: [showdownHighlight]
+});
+
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(find, "g"), replace);
+}
+
+export class MarkdownParser {
+  parse(md: string, url): string {
+    let filtered = replaceAll(md, "./img\\/", `img/`);
+    filtered = replaceAll(filtered, "img\\/", `https://${url}/img/`);
+    filtered = replaceAll(filtered, "./archives\\/", `archives/`);
+    filtered = replaceAll(filtered, "archives\\/", `https://${url}/archives/`);
+    filtered = replaceAll(filtered, "\\]\\(\\#", `](https://${url}#/`);
+    return converter.makeHtml(filtered);
+  }
+}
