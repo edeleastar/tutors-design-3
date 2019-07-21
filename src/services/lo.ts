0ecccdbb998dc08b4b70b4b29c4d53969cdd1629
Eamonn de Leastar <edeleastar@wit.ie>
2019-07-21 12:54:48 +0100

    Lo, Course and CourseRepo classes

---
+++ src/services/lo.ts
@@ -0,0 +1,16 @@
+export interface Lo {
+  properties: { [prop: string]: string };
+  version: string;
+  type: string;
+  shortTitle: string;
+  title: string;
+  img: string;
+  video: string;
+  pdf: string;
+  summary: string;
+  contentMd: string;
+  route: string;
+  id: string;
+  hide: boolean;
+  los: Lo[];
+}
---
+++ src/services/utils.ts
@@ -0,0 +1,13 @@
+import {Lo} from "./lo";
+
+export function injectCourseUrl(lo: Lo, url) {
+  if (lo.route) lo.route = lo.route.replace("{{COURSEURL}}", url);
+  if (lo.img) lo.img = lo.img.replace("{{COURSEURL}}", url);
+  if (lo.video) lo.video = lo.video.replace("{{COURSEURL}}", url);
+  if (lo.pdf) lo.pdf = lo.pdf.replace("{{COURSEURL}}", url);
+  if (lo.los) {
+    lo.los.forEach(lo => {
+      injectCourseUrl(lo, url);
+    });
+  }
+}
