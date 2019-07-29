47a57e3840eb7e6e71590cb5c87289c919807623
John Fitzgerald <usplitu@hotmail>
2019-07-29 19:38:02 +0100

    in progress: using router to write to address bar

--- src/components/search/search-view.ts
+++ src/components/search/search-view.ts
@@ -3,14 +3,16 @@ import { autoinject } from "aurelia-framework";
 import { Course } from "../../services/course";
 import { flattenedLos } from "../../services/utils-search";
 import { allLos } from "../../services/utils";
+import { Router } from "aurelia-router";
 
 @autoinject
 export class SearchView {
   course: Course;
   search_strings: string[] = [];
+  params: any;
   public searchTerm: string = "";
 
-  constructor(private courseRepo: CourseRepo) {}
+  constructor(private courseRepo: CourseRepo, private router: Router) {}
 
   public get searchTermInView() {
     return this.searchTerm;
@@ -22,45 +24,57 @@ export class SearchView {
   }
 
   public searchTermInViewChanged() {
-    this.updateUrl(this.searchTerm);
+    this.updateUrl2(this.searchTerm);
+    //this.updateUrl(this.searchTerm);
   }
 
-  async activate(params: any, ) {
+  async activate(params: any ) {
+    this.params = params;
     this.searchTerm = params['searchTerm'] != undefined ? params['searchTerm'] : "";
-    this.updateUrl(this.searchTerm);
+    //this.updateUrl(this.searchTerm);
+    this.updateUrl2(this.searchTerm);
     this.course = await this.courseRepo.fetchCourse(params.courseurl);
     this.setSearchStrings();
   }
 
+  updateUrl2(queryString: string) {
+    if(!queryString) return;
+    this.params.searchTerm=queryString;
+    this.router.navigateToRoute(
+      "search",
+      this.params,
+      { trigger: false, replace: true }
+      );
+  }
   /**
    * Live update the http url query string
    */
-  updateUrl(queryString: string) {
-    let href = window.location.href;
-    const indx = href.lastIndexOf("=");
+  // updateUrl(queryString: string) {
+  //   let href = window.location.href;
+  //   const indx = href.lastIndexOf("=");
 
-    //.com
-    if(!queryString && indx == -1) {
-      return;
-    }
+  //   //.com
+  //   if(!queryString && indx == -1) {
+  //     return;
+  //   }
 
-    // Valid queryString & no prior query string therefore create and populate.
-    if (queryString && indx == -1) {
-      href += "?searchTerm=" + queryString;
-    } 
+  //   // Valid queryString & no prior query string therefore create and populate.
+  //   if (queryString && indx == -1) {
+  //     href += "?searchTerm=" + queryString;
+  //   } 
 
-    //.com?searchTerm=
-    else if(!queryString && indx != -1) {
-      href = this.removeQueryString(href);
-    }
+  //   //.com?searchTerm=
+  //   else if(!queryString && indx != -1) {
+  //     href = this.removeQueryString(href);
+  //   }
 
-    // Replace completely the existing query string assuming queryString valid.
-    else if (queryString) {
-      href = href.replace(/(searchTerm=)[^\&]+/, "$1" + queryString);
-    }
+  //   // Replace completely the existing query string assuming queryString valid.
+  //   else if (queryString) {
+  //     href = href.replace(/(searchTerm=)[^\&]+/, "$1" + queryString);
+  //   }
 
-    window.location.href = href;
-  }
+  //   window.location.href = href;
+  // }
 
   private removeQueryString(href: string) : string {
     return href.substring(0, href.indexOf('?'));
