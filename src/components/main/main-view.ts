75f3af5a7644ad13d372ecd4685efa2d0ff9141b
Eamonn de Leastar <edeleastar@wit.ie>
2019-07-30 08:55:10 +0100

    correct lab view + coding style adjustments

--- src/components/main/main-view.ts
+++ src/components/main/main-view.ts
@@ -1,10 +1,8 @@
-import { NavigatorProperties } from "../../resources/elements/navigators/navigator-properties";
 import { autoinject } from "aurelia-framework";
-import {BaseView} from "../base/base-view";
+import { BaseView } from "../base/base-view";
 
 @autoinject
 export class MainView extends BaseView {
-
   async activate(params, route) {
     this.navigatorProperties.title = "Tutors Tuition System";
     this.navigatorProperties.subtitle = "Eamonn de Leastar, WIT Computing";
