122865af8eb114641b940ae98fc2684b7d15aba5
Eamonn de Leastar <edeleastar@wit.ie>
2019-08-29 11:11:46 +0100

    refactor env to include redirecturi for auth0 + firebase credentials

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -24,8 +24,7 @@ export class AuthService {
   auth0 = new WebAuth({
     domain: environment.auth0.domain,
     clientID: environment.auth0.clientId,
-    //redirectUri: "http://localhost:8080/authorize",
-    redirectUri: "https://tutors-design-dev.netlify.com/authorize",
+    redirectUri: environment.auth0.redirectUri,
     audience: `https://${environment.auth0.domain}/userinfo`,
     responseType: "token id_token",
     scope: "openid"
