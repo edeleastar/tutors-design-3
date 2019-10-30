9125ea0a3cda15226b46696df396f988ac2384da
John J. Fitzgerald <uspltu@hotmail.com>
2019-10-30 10:39:22 +0000

    partial fix

--- src/components/search/search-view.ts
+++ src/components/search/search-view.ts
@@ -30,22 +30,27 @@ export class SearchView extends BaseView {
 
   public searchTermInViewChanged() {
     this.updateUrl(this.searchTerm);
+    if (this.searchTerm) {
+      this.setSearchStrings();
+    }
   }
 
   async activate(params: any) {
     this.params = params;
-    this.searchTerm = params["searchTerm"] != undefined ? params["searchTerm"] : "";
-    this.updateUrl(this.searchTerm);
     this.course = await this.courseRepo.fetchCourse(params.courseurl);
-    this.setSearchStrings();
-
-    super.init(`search/${params.courseurl}`);
-
     this.navigatorProperties.title = "Search...";
     this.navigatorProperties.subtitle = this.course.lo.title;
     this.navigatorProperties.parentLink = `${environment.urlPrefix}/course/${this.courseRepo.courseUrl}`;
     this.navigatorProperties.parentIcon = "moduleHome";
     this.navigatorProperties.parentIconTip = "To module home ...";
+
+    this.searchTerm = params["searchTerm"] != undefined ? params["searchTerm"] : "";
+    this.updateUrl(this.searchTerm);
+    super.init(`search/${params.courseurl}`);
+
+    if (this.searchTerm) {
+      this.setSearchStrings();
+    }
   }
 
   /**
