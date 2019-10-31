8fb47b4d5d707dc268d8c0962b5f016d0c7a7cde
John J. Fitzgerald <uspltu@hotmail.com>
2019-10-31 09:49:47 +0000

    testing in progress

--- src/services/utils-search.ts
+++ src/services/utils-search.ts
@@ -6,8 +6,10 @@ const removeMd = require("remove-markdown");
 /**
  * Searches an array of nested Lo arrays for presence of searchTerm.
  * When a string containing the searchTerm is found it is converted to text.
- * It is clipped to a length obtained by adding extraChars number characters,
+ * It is augmented to a length obtained by adding extraChars number characters,
  * which is an environment variable, to the length of the searchTerm.
+ * On 31.10.19 resolved the issue whereby only first instance of a searchTerm on any
+ * web page returned. Now all instances of the search term identified.
  * @param los The nested arrays of Lo objects.
  * @param searchTerm The term whose presence is searched for.
  */
@@ -16,8 +18,10 @@ export function flattenedLos(los: Lo[], searchTerm: string): string[] {
   let result: string[] = [];
   flatLos.forEach(obj => {
     let text = removeMd(obj.lab.contentMd);
-    let content = clippedContent(text, searchTerm, extraChars);
-    result.push(`<a href="${obj.lab.route}">${obj.topicTitle}${obj.lab.title} ${obj.lab.shortTitle}</a>  ${content}`);
+    let contents = augmentedSubstrings(text, searchTerm, extraChars);
+    for (let content of contents) {
+      result.push(`<a href="${obj.lab.route}">${obj.topicTitle}${obj.lab.title} ${obj.lab.shortTitle}</a>  ${content}`);
+    }
   });
   return result;
 }
@@ -49,19 +53,61 @@ export function isValid(str: string) {
 }
 
 /**
- * Constructs a substring of targetString comprising searchTerm and
- * extraChars on either side of searchTerm.
- * A precondition is that searchTerm is a substring of targetString.
- * @param targetString
- * @param searchTerm
- * @param extraChars
+ * Returns an array of substrings of targetString comprising searchTerm and
+ * extraChars number characters on either side of searchTerm.
+ * That is, the total length of each substring is the sum of the 
+ * lengths of the search term + twice extra chars. However in the edge conditions
+ * namely at the beginning and end of the target string, any necessary adjustment is 
+ * made to substrings that might be present here or near here.
+ *
+ * Example: 	targetString  = 'syeasy and hard synchronized syncsy';
+ * searchTerm = 'sy';
+ * extraChars = 4;
+ * arStrings: ["syeasy", "syeasy and", "ard synchr", "zed syncsy", "syncsy"]
+ * 
+ * @param targetString The string within which one or more searchTerm substrings exist.
+ * @param searchTerm The substring, at lease one instance of which exists.
+ * @param extraChars The number of additional characters to be added each end of searchTerm, edge conditions allowing.
+ * @return arStrings An array of augmented substrings.
  */
-function clippedContent(targetString: string, searchTerm: string, extraChars: number) {
-  let index = targetString.indexOf(searchTerm);
-  let startIndex = index - extraChars > 0 ? index - extraChars : 0;
-  let endIndex =
-    index + searchTerm.length + extraChars <= targetString.length
-      ? index + searchTerm.length + extraChars
-      : targetString.length;
-  return targetString.slice(startIndex, endIndex);
+function augmentedSubstrings(targetString: string, searchTerm: string, extraChars: number) {
+  let arIndx = []; // The targetString indices of the first character of each occurrence of the substring.
+  let arStrings = []; // The output: the set of discovered and augmented substrigs.
+  arSubstringIndices(targetString, searchTerm, arIndx); // populate arIndx
+  for (let i = 0; i < arIndx.length; i += 1) {
+    let index = arIndx[i];
+    let startIndex = index - extraChars > 0 ? index - extraChars : 0;
+    let endIndex =
+      index + searchTerm.length + extraChars <= targetString.length
+        ? index + searchTerm.length + extraChars : targetString.length;
+    let str = targetString.slice(startIndex, endIndex);
+    arStrings.push(str);
+  }
+  return arStrings;
 }
+
+/**
+ *  Recursively locate the start indices of a recurring substring within a string.
+ *  Example: str = syeasy and hard synchronized syncsy'
+ *  Substring: 'sy'
+ *  Output: [0, 4, 16, 29, 33]
+ *  
+ *  @return arIndx An array of the indices of positions of first character of substring.
+ */
+
+function arSubstringIndices(str: string, substr: string, arIndx: number[]) {
+
+  let n = str.indexOf(substr);
+  if (n != -1) {
+    let prev_n = n;
+    if (arIndx.length) {
+      n += arIndx[arIndx.length - 1] + substr.length;
+    }
+    arIndx.push(n);
+    arSubstringIndices(str.slice(prev_n + substr.length), substr, arIndx);
+  }
+  else {
+    return arIndx;
+  }
+}
+
