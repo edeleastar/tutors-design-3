10b5558bc0c7c6ad3b22782ce786f73819d56e11
Eamonn de Leastar <edeleastar@wit.ie>
2020-04-29 14:47:48 +0100

    switched to css for icon colours

--- src/resources/elements/iconography/icon.ts
+++ src/resources/elements/iconography/icon.ts
@@ -1,16 +1,11 @@
-import { iconColours, faIcons } from "./styles";
+import { faIcons } from "./styles";
 import { bindable } from "aurelia-framework";
 
 export class Icon {
   @bindable type: string;
   @bindable size: string;
-  colour: string;
 
   icon(type: string) {
     return faIcons[type];
-  }
-
-  attached() {
-    this.colour = iconColours[this.type];
   }
 }
--- src/resources/elements/iconography/styles.ts
+++ src/resources/elements/iconography/styles.ts
@@ -25,36 +25,6 @@ import {
   faUserClock
 } from "@fortawesome/free-solid-svg-icons";
 
-export const iconColours = {
-  course: "#009688",
-  topic: "#009688",
-  talk: "#009688",
-  reference: "#009688",
-  lab: "#00BCD4",
-  archive: "#453F78",
-  panelvideo: "#F44336",
-  video: "#F44336",
-  adobeconnect: "grey",
-  slack: "#573852",
-  moodle: "#f87f2a",
-  github: "black",
-  youtube: "red",
-  moduleHome: "",
-  programHome: "",
-  toc: "",
-  film: "red",
-  web: "",
-  unit: "",
-  pdf: "red",
-  logout: "forestgreen",
-  tutorsTime: "#85144b",
-  labViewDetail: "",
-  labViewSummary: "",
-  labTimeDetail: "",
-  LabTimeSummary: "",
-  timeExport: "forestgreen"
-};
-
 export interface IconNav {
   link: string;
   icon: string;
