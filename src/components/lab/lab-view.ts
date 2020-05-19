9fbd70f78e5b7d6f9c9d99710eb5863072e9db97
Eamonn de Leastar <edeleastar@wit.ie>
2020-05-19 13:34:59 +0100

    rework lab to support vertical menu

--- src/components/lab/lab-view.ts
+++ src/components/lab/lab-view.ts
@@ -3,6 +3,8 @@ import environment from "../../environment";
 import { Lo } from "../../services/course/lo";
 import { BaseView } from "../base/base-view";
 import { NavigatorProperties } from "../../resources/elements/navigators/navigator-properties";
+import { faUikit } from "@fortawesome/free-brands-svg-icons";
+import { removeLeadingHashes } from "services/utils/utils";
 const path = require("path");
 
 export class LabView extends BaseView {
@@ -10,28 +12,56 @@ export class LabView extends BaseView {
   content = "";
   url = "";
   currentChapterShortTitle = "";
+  currentChapterTitle = "";
   navbarHtml = "";
   markdownParser = new MarkdownParser();
   chaptersHtml = new Map<string, string>();
+  chaptersTitles = new Map<string, string>();
   objectivesHtml = "";
+
+  vertical = false;
+
+  attached() {
+    if (localStorage.labVertical) {
+      if (localStorage.labVertical == "false") {
+        this.vertical = false;
+      } else {
+        this.vertical = true;
+      }
+    }
+    this.refreshav();
+  }
 
   refreshav() {
     this.navbarHtml = "";
-    this.lab.los.forEach(chapter => {
+    this.lab.los.forEach((chapter) => {
       const active = chapter.shortTitle == this.currentChapterShortTitle ? "class= uk-active" : "";
+      let title = "";
+      if (this.vertical) {
+        title = this.chaptersTitles.get(chapter.shortTitle);
+      } else {
+        title = chapter.shortTitle;
+      }
       this.navbarHtml = this.navbarHtml.concat(
-        `<li ${active}> <a href="${environment.urlPrefix}lab/${this.url}/${chapter.shortTitle}"> ${chapter.shortTitle} </a> </li>`
+        `<li ${active}> <a href="${environment.urlPrefix}lab/${this.url}/${chapter.shortTitle}"> ${title} </a> </li>`
       );
     });
+  }
+
+  onClickSwitch() {
+    this.vertical = !this.vertical;
+    this.refreshav();
+    localStorage.labVertical = this.vertical;
   }
 
   async fecthLab() {
     if (!this.lab) {
       this.lab = await this.courseRepo.fetchLab(this.url);
-      this.objectivesHtml = this.markdownParser.parse(this.lab.los[0].contentMd, this.url)
-      this.lab.los.forEach(chapter => {
-        this.chaptersHtml.set(chapter.shortTitle, this.markdownParser.parse(chapter.contentMd, this.url))
-      })
+      this.objectivesHtml = this.markdownParser.parse(this.lab.los[0].contentMd, this.url);
+      this.lab.los.forEach((chapter) => {
+        this.chaptersHtml.set(chapter.shortTitle, this.markdownParser.parse(chapter.contentMd, this.url));
+        this.chaptersTitles.set(chapter.shortTitle, removeLeadingHashes(chapter.title));
+      });
     }
   }
   async activate(params) {
@@ -40,15 +70,18 @@ export class LabView extends BaseView {
       this.url = params.laburl;
       await this.fecthLab();
       this.currentChapterShortTitle = this.lab.los[0].shortTitle;
+      this.currentChapterTitle = this.chaptersTitles.get(this.lab.los[0].shortTitle);
       this.content = this.objectivesHtml;
     } else {
       this.url = path.dirname(params.laburl);
       await this.fecthLab();
       this.currentChapterShortTitle = lastSegment;
+      this.currentChapterTitle = this.chaptersTitles.get(lastSegment);
       this.content = this.chaptersHtml.get(lastSegment);
     }
     this.refreshav();
     super.init(`lab/${params.laburl}`, this.lab);
+    this.navSubtitle(this.currentChapterTitle);
   }
 
   configMainNav(nav: NavigatorProperties) {
@@ -60,7 +93,7 @@ export class LabView extends BaseView {
         companions: false,
         walls: true,
         tutorsTime: false,
-        toc: true
+        toc: true,
       },
       {
         title: this.lab.title,
@@ -68,9 +101,8 @@ export class LabView extends BaseView {
         img: this.lab.img,
         parentLink: this.lab.parent.lo.route,
         parentIcon: "topic",
-        parentTip: "To parent topic ..."
+        parentTip: "To parent topic ...",
       }
     );
   }
 }
-
