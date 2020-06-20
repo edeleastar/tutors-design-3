80b9540a8faa310fde2ee2173f065f2d1cad6850
Eamonn de Leastar <edeleastar@wit.ie>
2020-06-20 17:28:07 +0100

    remove tutors time excess sheets

--- src/components/time/sheets/lab-count-sheet.ts
+++ src/components/time/sheets/lab-count-sheet.ts
@@ -1,11 +1,11 @@
 import { Lo } from "../../../services/course/lo";
 import { LabSheet } from "./lab-sheet";
 import { deepScheme } from "./heat-map-colours";
-import { UserMetric } from "../../../services/events/event-definitions";
+import { UserMetric } from "services/events/event-definitions";
 
-export class LabsTimeSummarySheet extends LabSheet {
-  title = "Lab Time in Minutes - Totals";
-  subtitle = "Total Number of Minutes for each lab";
+export class LabCountSheet extends LabSheet {
+  title = "Tutors Time";
+  subtitle = "Number of minutes a lab is active";
 
   populateCols(los: Lo[]) {
     los.forEach(lab => {
@@ -23,38 +23,20 @@ export class LabsTimeSummarySheet extends LabSheet {
   populateRow(user: UserMetric, los: Lo[]) {
     let row = this.creatRow(user);
     this.zeroEntries(los, row);
-
     let summaryCount = 0;
     user.labActivity.forEach(labMetric => {
       let labSummaryCount = 0;
       if (labMetric) {
         labMetric.metrics.forEach(stepMetric => {
-          if (stepMetric.duration) {
-            labSummaryCount = labSummaryCount + stepMetric.duration / 2;
-          }
+          if (stepMetric.count) labSummaryCount = labSummaryCount + stepMetric.count;
         });
+        labSummaryCount = Math.round(labSummaryCount / 2);
         row[`${labMetric.title}`] = labSummaryCount;
       }
       summaryCount = summaryCount + labSummaryCount;
     });
+
     row.summary = summaryCount;
     this.rowData.push(row);
-  }
-
-  updateRow(user: UserMetric, rowNode) {
-    let summaryCount = 0;
-    user.labActivity.forEach(labMetric => {
-      let labSummaryCount = 0;
-      if (labMetric) {
-        labMetric.metrics.forEach(stepMetric => {
-          if (stepMetric.duration) {
-            labSummaryCount = labSummaryCount + stepMetric.duration / 2;
-          }
-        });
-        rowNode.setDataValue(`${labMetric.title}`, labSummaryCount);
-      }
-      summaryCount = summaryCount + labSummaryCount;
-    });
-    rowNode.setDataValue("summary", summaryCount);
   }
 }
